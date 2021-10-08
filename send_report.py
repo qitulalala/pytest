b8092fc8f745cd2c2c8908e23cd1fbce50f8c5a6
unknown <2934679630@qq.com>
2021-10-08 18:35:43 +0800

    new005

--- send_report.py
+++ send_report.py
@@ -5,7 +5,7 @@ import requests
 
 class DingRobot:
     def __init__(self):
-        self.allure = "http://2934679630:Mm123456@47.93.5.75:9000/job/helloword/allure/widgets/suites.json"
+        self.allure = "http://jenkins5:2934679630:Mm123456@47.93.5.75:9000/job/helloword/allure/widgets/suites.json"
         self.ding = 'https://oapi.dingtalk.com/robot/send?access_token=' \
                     '312d2a9b69969e261495b8927067bbf18b1874344d7a7d85d35e5677e08a6de5'
         self.error = self.get_allure()
@@ -21,13 +21,12 @@ class DingRobot:
             content = {
                 "msgtype": "link",
                 "link": {
-                    "text": "账号jenkisn5,密码xxxxx",
-                    "title": "qitulalala" + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                    "picUrl": "",
-                    "messageUrl": "http://2934679630:Mm123456@47.93.5.75:9000/job/helloword/allure"
+                    "text": "用户：2934679630.密码：xxxx",
+                    "title": "大撒法" + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                    "messageUrl": "http:///jenkins5:2934679630:Mm123456@47.93.5.75:9000/job/helloword/allure"
                 }
             }
-            response = requests.post(self.ding, headers=headers, data=json.dumps(content))
+            requests.post(self.ding, headers=headers, data=json.dumps(content))
         else:
             print('无报错')
 
